9fbab403977e4060cd13d070e3ea25a7fc8e922e
IgniteConvention <ignitestudentconvention@gmail.com>
2025-01-13 07:57:21 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Welcome to React</h1>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Registration App</h1>
+        <p>This is your starting point. Edit <code>src/App.js</code> to customize.</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
-
