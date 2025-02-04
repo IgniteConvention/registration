c1daef417b565fbc0b5155042a95b83e4417430c
IgniteConvention <ignitestudentconvention@gmail.com>
2025-02-04 10:28:59 -0600

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { loginUser } from "../auth";
 
 const LoginPage = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -23,6 +25,7 @@ const LoginPage = ({ onLogin }) => {
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <button type="submit">Login</button>
       </form>
+      <p>Don't have an account? <button onClick={() => navigate("/register")}>Register</button></p>
     </div>
   );
 };
