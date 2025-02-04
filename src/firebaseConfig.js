a6543bcb5ff047e45e819358384c7b7b7584df82
IgniteConvention <ignitestudentconvention@gmail.com>
2025-02-04 08:57:01 -0600

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUnjhtcDnGdLsxgNMD5ZTrk4qstOuRwpM",
+  authDomain: "igniteregistration.firebaseapp.com",
+  projectId: "igniteregistration",
+  storageBucket: "igniteregistration.appspot.com",
+  messagingSenderId: "365150542866",
+  appId: "1:365150542866:web:736202288ba4c7aaf70ccb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
